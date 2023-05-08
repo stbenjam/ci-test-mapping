61b8a8fb5f6e37cbd0831f83eaba0e65a4c175a1
Stephen Benjamin <stephen@bitbin.de>
2023-05-08 13:12:06 -0400

    Verify components have mapping

---
+++ cmd/ci-test-mapping/verify.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/util/sets"
+
+	"github.com/openshift-eng/ci-test-mapping/pkg/registry"
+)
+
+type VerifyFlags struct {
+	JiraURL string
+}
+
+func NewVerifyFlags() *VerifyFlags {
+	return &VerifyFlags{
+		JiraURL: "https://issues.redhat.com/rest/api/2/issue/createmeta/OCPBUGS/issuetypes/1",
+	}
+}
+
+var verifyCmd = &cobra.Command{
+	Use:   "verify",
+	Short: "Verify all JIRA components are represented in ci-test-mapping",
+	Run: func(cmd *cobra.Command, args []string) {
+		f := NewVerifyFlags()
+
+		logrus.Info("fetching jira components from jira...")
+		bearerToken := os.Getenv("JIRA_TOKEN")
+		if bearerToken == "" {
+			cmd.Usage() // nolint:errcheck
+			logrus.Fatal("jira token required")
+		}
+		jiraComponents, err := getJiraComponents(f.JiraURL, bearerToken)
+		if err != nil {
+			logrus.WithError(err).Fatal("could not fetch jira components")
+		}
+
+		logrus.Info("verifying all components have a mapping")
+		reg := registry.NewComponentRegistry()
+		mappedJiraComponents := sets.New[string]()
+		for _, c := range reg.Components {
+			mappedJiraComponents.Insert(c.JiraComponents()...)
+		}
+
+		for _, component := range jiraComponents {
+			if !mappedJiraComponents.Has(component) {
+				logrus.WithFields(logrus.Fields{
+					"path":    "pkg/" + getPackagePath(component),
+					"package": getPackageName(component),
+				}).Warningf("no mapping for jira component %q", component)
+			}
+		}
+
+		jiraComponentsSet := sets.New[string](jiraComponents...)
+		for _, mappedComponent := range mappedJiraComponents.UnsortedList() {
+			if !jiraComponentsSet.Has(mappedComponent) && mappedComponent != "" {
+				logrus.Errorf("unknown component %q not found in jira", mappedComponent)
+			}
+		}
+
+		logrus.Info("done!")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(verifyCmd)
+}
