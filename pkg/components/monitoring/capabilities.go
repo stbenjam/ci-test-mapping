674575ca42b45a9e0c5ba05a5152eb0c2a2ec994
Stephen Benjamin <stephen@redhat.com>
2024-01-19 08:50:23 -0500

    TRT-1351: Add Disruption capability to disruption tests
    Add a disruption capability to all disruption tests.

--- pkg/components/monitoring/capabilities.go
+++ pkg/components/monitoring/capabilities.go
@@ -8,14 +8,7 @@ import (
 )
 
 func identifyCapabilities(test *v1.TestInfo) []string {
-	var capabilities []string
-
-	// Get the Feature name from the test name as a capability
-	capabilities = append(capabilities, util.ExtractTestField(test.Name, "Feature")...)
-
-	if strings.Contains(test.Name, "clusteroperator/") {
-		capabilities = append(capabilities, "Operator")
-	}
+	capabilities := util.DefaultCapabilities(test)
 
 	if strings.Contains(test.Name, "alert/") || strings.Contains(test.Name, "Alerts") || strings.Contains(test.Name, "alerting") {
 		capabilities = append(capabilities, "Alerts")
--- pkg/components/node/numaawarescheduling/capabilities.go
+++ pkg/components/node/numaawarescheduling/capabilities.go
@@ -1,25 +1,12 @@
 package nodenumaawarescheduling
 
 import (
-	"strings"
-
 	v1 "github.com/openshift-eng/ci-test-mapping/pkg/api/types/v1"
 	"github.com/openshift-eng/ci-test-mapping/pkg/util"
 )
 
 func identifyCapabilities(test *v1.TestInfo) []string {
-	var capabilities []string
-
-	// Get the Feature name from the test name as a capability
-	capabilities = append(capabilities, util.ExtractTestField(test.Name, "Feature")...)
-
-	if strings.Contains(test.Name, "clusteroperator/") {
-		capabilities = append(capabilities, "Operator")
-	}
-
-	if strings.Contains(test.Name, "alert/") {
-		capabilities = append(capabilities, "Alerts")
-	}
+	capabilities := util.DefaultCapabilities(test)
 
 	return capabilities
 }
