99cc525ab48649bd337236b9dc96dbb7c58c752e
Petr Muller <muller@redhat.com>
2024-06-18 20:05:45 +0200

    OTA-1243: Sort `Other` CVO tests to capabilities
    In addition to the existing `ClusterUpgrade` capability, introduce new `ClusterOperators`, `Operator` and `AdminAck` capabilities to cover what CVO does.

--- pkg/components/clusterversionoperator/capabilities.go
+++ pkg/components/clusterversionoperator/capabilities.go
@@ -1,12 +1,46 @@
 package clusterversionoperator
 
 import (
+	"regexp"
+
 	v1 "github.com/openshift-eng/ci-test-mapping/pkg/api/types/v1"
 	"github.com/openshift-eng/ci-test-mapping/pkg/util"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
-func identifyCapabilities(test *v1.TestInfo) []string {
-	capabilities := util.DefaultCapabilities(test)
+const (
+	// ClusterUpgrade is a CVO capability to upgrade the cluster
+	ClusterUpgrade = "ClusterUpgrade"
+	// ClusterOperators is a CVO capability to manage and monitor Cluster Operators
+	ClusterOperators = "ClusterOperators"
+	// Operator is a CVO capability to operate the cluster both during and outside of upgrades
+	Operator = "Operator"
+	// AdminAck is a CVO capability to process administrator acknowledgements
+	AdminAck = "AdminAck"
+)
 
-	return capabilities
+var cvoCapabilitiesIdentifiers = map[*regexp.Regexp]string{
+	regexp.MustCompile(`.*upgrade.*`): ClusterUpgrade,
+
+	// e.g. [sig-cluster-lifecycle] pathological event should not see excessive Back-off restarting failed containers for ns/openshift-cluster-version
+	regexp.MustCompile(".*ns/openshift-cluster-version.*"): Operator,
+	// all invariant tests
+	// e.g. [Jira:"Cluster Version Operator"] monitor test legacy-cvo-invariants collection
+	regexp.MustCompile(".*monitor test.*"): Operator,
+
+	// e.g. Cluster upgrade.[sig-cluster-lifecycle] ClusterOperators are available and not degraded after upgrade
+	regexp.MustCompile(".*ClusterOperators.*"): ClusterOperators,
+
+	regexp.MustCompile(`.*(admin ack|AdminAck).*`): AdminAck,
+}
+
+func identifyCapabilities(test *v1.TestInfo) []string {
+	capabilities := sets.New[string](util.DefaultCapabilities(test)...)
+	for matcher, capability := range cvoCapabilitiesIdentifiers {
+		if matcher.MatchString(test.Name) {
+			capabilities.Insert(capability)
+		}
+	}
+
+	return capabilities.UnsortedList()
 }
