e831138e7a14ee407f7c7d96005edb8af8ac06f1
Stephen Benjamin <stephen@redhat.com>
2024-07-24 08:32:36 -0400

    TRT-1727: fix mapping verify

--- cmd/ci-test-mapping/mapping_verify.go
+++ cmd/ci-test-mapping/mapping_verify.go
@@ -10,90 +10,97 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
 	v1 "github.com/openshift-eng/ci-test-mapping/pkg/api/types/v1"
 	"github.com/openshift-eng/ci-test-mapping/pkg/components"
 )
 
+var verifyMapFlags = NewVerifyMapFlags()
+var mappingFiles = []string{"data/openshift-gce-devel/ci_analysis_us/component_mapping.json"}
+
 type VerifyMapFlags struct {
-	CurrentMapURL string
-	NewMapFile    string
+	MappingFiles []string
 }
 
 func NewVerifyMapFlags() *VerifyMapFlags {
-	return &VerifyMapFlags{
-		CurrentMapURL: "https://raw.githubusercontent.com/openshift-eng/ci-test-mapping/main/mapping.json",
-		NewMapFile:    "mapping.json",
-	}
+	return &VerifyMapFlags{}
+}
+
+func (f *VerifyMapFlags) BindFlags(fs *pflag.FlagSet) {
+	fs.StringArrayVar(&f.MappingFiles, "mapping-files", mappingFiles, "Which mapping files to verify")
 }
 
 var verifyMapCmd = &cobra.Command{
 	Use:   "map-verify",
 	Short: "Verify mapping is correct, i.e. no tests are moving from assigned to unknown",
 	Run: func(cmd *cobra.Command, args []string) {
-		f := NewVerifyMapFlags()
 
 		now := time.Now()
 		logrus.Infof("verifying mappings are correct...")
 
-		// **** Get the current mappings
-		response, err := http.Get(f.CurrentMapURL)
-		if err != nil {
-			fmt.Println("Failed to fetch data:", err)
-			return
-		}
-		defer response.Body.Close()
-		currentMap, err := fetchMap(response.Body)
-		if err != nil {
-			logrus.WithError(err).Fatalf("couldn't fetch current mapping")
-		}
+		for _, mf := range verifyMapFlags.MappingFiles {
+			url := fmt.Sprintf("https://raw.githubusercontent.com/openshift-eng/ci-test-mapping/main/%s", mf)
 
-		// Create current mapping lookup table
-		type cl struct {
-			name  string
-			suite string
-		}
-		currentComponents := make(map[cl]string)
-		for _, mapping := range currentMap {
-			currentComponents[cl{mapping.Name, mapping.Suite}] = mapping.Component
-		}
-
-		// **** Get the mappings from the file
-		file, err := os.Open(f.NewMapFile)
-		if err != nil {
-			fmt.Println("Failed to open file:", err)
-			return
-		}
-		defer file.Close()
-		newMap, err := fetchMap(file)
-		if err != nil {
-			logrus.WithError(err).Fatalf("couldn't read new mapping from file")
-		}
-
-		// Look for removed mappings
-		removedMaps := make([]cl, 0)
-		for _, n := range newMap {
-			if n.Component != components.DefaultComponent {
-				continue
+			// **** Get the current mappings
+			response, err := http.Get(url)
+			if err != nil {
+				fmt.Println("Failed to fetch data:", err)
+				return
 			}
-			locator := cl{n.Name, n.Suite}
-			if old, ok := currentComponents[locator]; ok && old != components.DefaultComponent {
-				removedMaps = append(removedMaps, locator)
+			defer response.Body.Close()
+			currentMap, err := fetchMap(response.Body)
+			if err != nil {
+				logrus.WithError(err).Fatalf("couldn't fetch current mapping")
 			}
-		}
 
-		logrus.Infof("verification complete in %+v", time.Since(now))
-		if len(removedMaps) == 0 {
-			return
-		}
+			// Create current mapping lookup table
+			type cl struct {
+				name  string
+				suite string
+			}
+			currentComponents := make(map[cl]string)
+			for _, mapping := range currentMap {
+				currentComponents[cl{mapping.Name, mapping.Suite}] = mapping.Component
+			}
 
-		for _, removed := range removedMaps {
-			logrus.WithFields(logrus.Fields{
-				"Name":  removed.name,
-				"Suite": removed.suite,
-			}).Warningf("test moved from %q to \"Unknown\"", currentComponents[removed])
+			// **** Get the mappings from the file
+			file, err := os.Open(mf)
+			if err != nil {
+				fmt.Println("Failed to open file:", err)
+				return
+			}
+			defer file.Close()
+			newMap, err := fetchMap(file)
+			if err != nil {
+				logrus.WithError(err).Fatalf("couldn't read new mapping from file")
+			}
+
+			// Look for removed mappings
+			removedMaps := make([]cl, 0)
+			for _, n := range newMap {
+				if n.Component != components.DefaultComponent {
+					continue
+				}
+				locator := cl{n.Name, n.Suite}
+				if old, ok := currentComponents[locator]; ok && old != components.DefaultComponent {
+					removedMaps = append(removedMaps, locator)
+				}
+			}
+
+			logrus.Infof("verification complete in %+v", time.Since(now))
+			if len(removedMaps) == 0 {
+				return
+			}
+
+			for _, removed := range removedMaps {
+				logrus.WithFields(logrus.Fields{
+					"Name":  removed.name,
+					"Suite": removed.suite,
+				}).Warningf("test moved from %q to \"Unknown\"", currentComponents[removed])
+			}
+			logrus.Fatalf("Components are not allowed to move to Unknown. Please assign correct ownership.")
 		}
-		logrus.Fatalf("Components are not allowed to move to Unknown. Please assign correct ownership.")
 	},
 }
 
@@ -113,5 +120,6 @@ func fetchMap(f io.Reader) ([]v1.TestOwnership, error) {
 }
 
 func init() {
+	verifyMapFlags.BindFlags(verifyMapCmd.Flags())
 	rootCmd.AddCommand(verifyMapCmd)
 }
